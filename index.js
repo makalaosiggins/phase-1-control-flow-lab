60fbe58d362adb4c09b8cb99717fdc79368ffcf5
makalaosiggins <wrxmakala@icloud.com>
2024-05-17 19:58:54 -0600

    completed lab fixed

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-function scuberGreetingForFeet(distance){
+function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
     return "This one is on me!";
-  } else if (distance > 400 && distance <= 2000) {
+ } else if (distance > 400 && distance <= 2000) {
     return "That will be twenty bucks.";
-  } else if (distance > 2000 && distance < 2500) {
+ } else if (distance > 2000 && distance <= 2500) {
     return "I will gladly take your thirty bucks.";
-  } else if (distance >= 2500) {
-    return "No can do."
+ } else if (distance > 2500) {
+    return "No can do.";
+ }
   }
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
 
-function ternaryCheckCity(city){
-  if (city === "NYC") {
-    return "Ok, sounds good.";
-  } else if (city !== "NYC") {
-    return "No go.";
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye."
   }
 }
-
-function switchOnCharmFromTip(tip){
-  if (tip === "generous") {
-    return "Thank you so much.";
-  } else if (tip === "not as generous") {
-    return "Thank you.";
-  } else {
-    return "Bye.";
-  }
-    }
